6af48e36954c8951f82e0ec06f999bf6fc537c8e
TolikRogov <tolik_rogov@bk.ru>
2024-08-27 14:50:59 +0300

    triangle

--- source/main.cpp
+++ source/main.cpp
@@ -2,17 +2,23 @@
 #include <assert.h>
 #include <stdlib.h>
 
-void Print(int data[][4], size_t size_x, size_t size_y) {
+void Print(int* data, size_t size_x, size_t size_y) {
+
+	size_t sm = 1;
+
 	for (size_t i = 0; i < size_x; i++) {
 
 		assert( (0 <= i && i < size_x) );
 
-		for (size_t j = 0; j < size_y; j++) {
+		for (size_t j = 0; j < sm; j++) {
 
 			assert( (0 <= j && j < size_y) );
 
-			printf("data[%lu][%lu] = %d ", i, j, data[i][j]);
+			printf("data[%lu][%lu] = %d ", i, j, *(data + i * size_x + j));
 		}
+
+		sm++;
+
 		printf("\n");
 	}
 }
@@ -22,13 +28,12 @@ int main() {
 	int data[][4] = { {11, 12, 13, 14},
 					  {21, 22, 23, 24},
 					  {31, 32, 33, 34},
-					  {41, 42, 43, 44},
-					  {51, 52, 53, 54} };
+					  {41, 42, 43, 44} };
 
 	size_t size_x = (sizeof(data) / sizeof(data[0])),
 		   size_y = (sizeof(data[0]) / sizeof(data[0][0]));
 
-	Print(data, size_x, size_y);
+	Print((int*)data, size_x, size_y);
 
 	return 0;
 }
